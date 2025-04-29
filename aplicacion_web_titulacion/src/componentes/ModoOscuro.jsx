de05fe5c78a00dc17745090e1b7d06449fbe05ce
ferx666g <fer1567g@gmail.com>
2025-04-28 22:55:29 -0500

    Actividades Sprint 1: Modificación botón modo oscuro

--- aplicacion_web_titulacion/src/componentes/ModoOscuro.jsx
+++ aplicacion_web_titulacion/src/componentes/ModoOscuro.jsx
@@ -31,9 +31,9 @@ export const ModoOscuro = () => {
   return (
     <button
       onClick={toggleModoOscuro}
-      className="mt-4 px-4 py-2 rounded-lg bg-gray-200 dark:bg-gray-700 text-black dark:text-white shadow"
+      className="fixed bottom-3  right-1 z-50 px-4 py-2 rounded-lg text-4xl   text-black dark:text-white shadow transition-all"
     >
-      {isDarkMode ? 'Modo Claro ☀️' : 'Modo Oscuro 🌙'}
+      {isDarkMode ? '☀️' : '🌙'}
     </button>
   );
 };
